49b92826e0358c8e8ad33aac1e19150186575d91
Anna Kurylo <anna.kurilo21@gmail.com>
2017-12-25 13:06:46 +0200

    add mock data

---
+++ src/data/deals.js
@@ -0,0 +1,244 @@
+const deals = [
+  {
+    '_id': '5a155f3df9eeba0de3802204',
+    'name': ': №ОЕ29 - модуль Z2 (1 шт.)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-20T00:00:00.000Z',
+    'endDate': '2017-12-21T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a2fe419f33f183e7e149086',
+    'name': ': ОЕ-49 Кресло Q1 (1шт)',
+    'status': 'В работе',
+    'priority': 3,
+    'startDate': '2017-12-12T00:00:00.000Z',
+    'endDate': '2018-01-16T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d7cabe490104b89eed4d4',
+    'name': ': ОЕ36 Барный стул Е1 (3шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-27T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a01958255561e0c9598c73f',
+    'name': ': №ОЕ20 _ Барный стул Е1 (1 шт.)',
+    'status': 'Выполнено',
+    'priority': null,
+    'startDate': '2017-11-06T00:00:00.000Z',
+    'endDate': '2017-12-05T00:00:00.000Z',
+    'endDateActual': '2017-12-01T00:00:00.000Z',
+    'closingDate': '2017-12-01T00:00:00.000Z'
+  },
+  {
+    '_id': '5a09ac43cfc8e6296403dbc7',
+    'name': ': №ОЕ24 _ Кресло Q1 (2 шт.), отаманка Q2 (1 шт.)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-10T00:00:00.000Z',
+    'endDate': '2017-12-12T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a26949455ab3a239e988067',
+    'name': ': ОЕ-41 Кресло Q1 black (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-30T00:00:00.000Z',
+    'endDate': '2018-01-05T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a26953455ab3a239e98806b',
+    'name': ': ОЕ-42 Столик N2 (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-12-01T00:00:00.000Z',
+    'endDate': '2017-12-29T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a26929855ab3a239e98805d',
+    'name': ': ОЕ-38 Лампа 15.77 М (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-29T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a0d856c68dea50d9422c657',
+    'name': ': №ОЕ26 _ кресло V1 (2 шт.), диван E7 (1 шт.)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-15T00:00:00.000Z',
+    'endDate': '2017-12-19T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a0d85e668dea50d9422c659',
+    'name': ': №ОЕ27 _ стул Е2 (18 шт.)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-16T00:00:00.000Z',
+    'endDate': '2017-12-19T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a155d73f9eeba0de38021fe',
+    'name': ': №ОЕ25 - кресло Q1 (1 шт.), оттоманка Q2 (1 шт.)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-14T00:00:00.000Z',
+    'endDate': '2017-12-14T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a09aba4cfc8e6296403dbc3',
+    'name': ': №ОЕ23 _ Кресло Q1 (1 шт.)',
+    'status': 'В работе',
+    'priority': 7,
+    'startDate': '2017-11-10T00:00:00.000Z',
+    'endDate': '2017-12-12T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d774be490104b89eed4bf',
+    'name': ': №ОЕ31 Стол N1 (2шт), Столик N2 (2шт), Столик N3 (3шт), Кресло Q1 black (1шт), Оттоманка Q2 (3шт), Стул Е1 (1шт), Стул Е3 (2шт), Комплект Set 2 (1шт), кресло Q1 (1шт)  ',
+    'status': 'В работе',
+    'priority': 3,
+    'startDate': '2017-11-23T00:00:00.000Z',
+    'endDate': '2017-12-26T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d7a5ae490104b89eed4c7',
+    'name': ': ОЕ32 - Диван Е8',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-22T00:00:00.000Z',
+    'endDate': '2017-12-26T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d7c31e490104b89eed4d0',
+    'name': ': ОЕ35 Кресло Q1 (1шт), Оттоманка Q2 (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-27T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d7dfbe490104b89eed4d8',
+    'name': ': ОЕ37 Стул Е16 (1шт) , Стул Е2 (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-27T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a2693dd55ab3a239e988062',
+    'name': ': ОЕ-39 Барный стул С5 (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-29T00:00:00.000Z',
+    'endDate': '2017-12-29T00:00:00.000Z',
+    'endDateActual': '2017-12-01T00:00:00.000Z',
+    'closingDate': null
+  },
+  {
+    '_id': '5a1d7b5ae490104b89eed4cc',
+    'name': ': ОЕ34 Столик Q3 (1шт)',
+    'status': 'В работе',
+    'priority': null,
+    'startDate': '2017-11-27T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a27f0ce2e84d71fe4fc0190',
+    'name': ': ОЕ-43 Тумбочка Н1 (2шт)',
+    'status': 'В работе',
+    'priority': 2,
+    'startDate': '2017-12-06T00:00:00.000Z',
+    'endDate': '2017-12-28T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a09aae9cfc8e6296403dbc0',
+    'name': ': №ОЕ22 _ Скамья В1 (1 шт.)',
+    'status': 'Выполнено',
+    'priority': null,
+    'startDate': '2017-11-10T00:00:00.000Z',
+    'endDate': '2017-12-12T00:00:00.000Z',
+    'endDateActual': '2017-12-06T00:00:00.000Z',
+    'closingDate': '2017-12-06T00:00:00.000Z'
+  },
+  {
+    '_id': '5a0192fa55561e0c9598c73a',
+    'name': ': №ОЕ19 _ Стул Е3 (1 шт.)',
+    'status': 'Выполнено',
+    'priority': 2,
+    'startDate': '2017-11-02T00:00:00.000Z',
+    'endDate': '2017-12-05T00:00:00.000Z',
+    'endDateActual': '2017-12-01T00:00:00.000Z',
+    'closingDate': '2017-12-07T00:00:00.000Z'
+  },
+  {
+    '_id': '5a0196ae55561e0c9598c745',
+    'name': ': Кресло V1 (5 шт.)',
+    'status': 'Выполнено',
+    'priority': 5,
+    'startDate': '2017-11-06T00:00:00.000Z',
+    'endDate': '2017-12-08T00:00:00.000Z',
+    'endDateActual': '2017-12-07T00:00:00.000Z',
+    'closingDate': '2017-12-07T00:00:00.000Z'
+  },
+  {
+    '_id': '5a2fe386f33f183e7e149081',
+    'name': ': ОЕ-48 Диван Е8 (5шт), Кресло Q1 (1шт), Оттоманка Q2 (1шт)',
+    'status': 'В работе',
+    'priority': 4,
+    'startDate': '2017-12-12T00:00:00.000Z',
+    'endDate': '2018-01-16T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  },
+  {
+    '_id': '5a09a907cfc8e6296403dbbc',
+    'name': ': №ОЕ21 _ Стол SP1 (1 шт.), комод SP2 (1 шт.)',
+    'status': 'В работе',
+    'priority': 3,
+    'startDate': '2017-11-07T00:00:00.000Z',
+    'endDate': '2017-12-14T00:00:00.000Z',
+    'endDateActual': null,
+    'closingDate': null
+  }
+]
+
+export default deals
