a07b996bf61c88272985619765efbaf059127a4b
Anna Kurylo <anna.kurilo21@gmail.com>
2017-12-25 12:47:43 +0200

    replace HelloWorld.vue with basic DealsDiagram component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <DealsDiagram/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import DealsDiagram from './components/DealsDiagram';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    DealsDiagram,
   },
 };
 </script>
